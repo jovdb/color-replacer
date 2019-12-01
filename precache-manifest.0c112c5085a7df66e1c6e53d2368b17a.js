39398135e163404da9d1e1d34be7718620cdbfc2
Jo Van den Berghe <jo.van.den.berghe@gmail.com>
2019-12-01 23:09:30 +0100

    Updates

---
+++ precache-manifest.0c112c5085a7df66e1c6e53d2368b17a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0afde335f733ba9cae4a13fbc94ad1d6",
+    "url": "/color-replacer/index.html"
+  },
+  {
+    "revision": "dfa010b130e84f44aeb4",
+    "url": "/color-replacer/static/css/main.ea10709e.chunk.css"
+  },
+  {
+    "revision": "8821897bd35d8418bb99",
+    "url": "/color-replacer/static/js/2.dcb280f3.chunk.js"
+  },
+  {
+    "revision": "dfa010b130e84f44aeb4",
+    "url": "/color-replacer/static/js/main.8d0a2d37.chunk.js"
+  },
+  {
+    "revision": "76c6ea9acd0279555c0a",
+    "url": "/color-replacer/static/js/runtime-main.93b57868.js"
+  }
+]);
